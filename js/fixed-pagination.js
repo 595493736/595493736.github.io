5f6a9e68223b429da127c76e54b0f76c1d087ec8
wj595493736 <>
2023-12-15 17:56:35 +0800

    Site updated: 2023-12-15 17:56:29

---
+++ js/fixed-pagination.js
@@ -0,0 +1,21 @@
+document.addEventListener('scroll',function(){
+  //滚动条高度+视窗高度 = 可见区域底部高度
+  var visibleBottom = window.scrollY + document.documentElement.clientHeight;
+  //可见区域顶部高度
+  var visibleTop = window.scrollY;
+  // 获取翻页按钮容器
+  var pagination = document.getElementById('pagination');
+  // 获取位置监测容器，此处采用评论区
+  // var eventlistner = document.getElementById('post-comment');
+  var footer = document.getElementById('footer');
+  // if (eventlistner&&pagination){
+  if (footer&&pagination){
+    // var centerY = eventlistner.offsetTop+(eventlistner.offsetHeight/2);
+	var centerY = footer.offsetTop+(footer.offsetHeight/2);
+    if(centerY>visibleTop&&centerY<visibleBottom){
+      pagination.style.display = 'flex';
+    }else{
+      pagination.style.display = 'none';
+    }
+  }
+})
